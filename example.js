fa949b6b1822d5ec5bc137e4bfffc77c2f8fbe53
Soldy <4786022+Soldy@users.noreply.github.com>
2021-06-13 22:52:38 +0100

    quit function to example
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- example.js
+++ example.js
@@ -5,6 +5,11 @@ watch.add(
    ['a','b','c','d','e','f','g','h','i'],
    (e)=>{console.log(e+' pressed');}
 );
+watch.add(
+   ['q'],
+   (e)=>{process.exit();}
+);
+
 
 watch.watch();
 setTimeout(()=>{},6000)
